5c3725102870be980c1499d12bca3c65d8b31f82
Varshinee Sreekanth <varshinee@utexas.edu>
2022-04-11 18:29:48 -0500

    Added pie chart for question breakdown

--- question_analysis.py
+++ question_analysis.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 import json
+import matplotlib.pyplot as plt
 
 def generate_question_counts(annotations):
     sum = 0
@@ -33,6 +34,24 @@ def generate_balanced_subset(total_types, total_sum, subset_types, subset_sum):
     with open("subset_questions_white", "w") as f:
         json.dump(questions_to_keep, f)
 
+'''
+Creates a pie chart showing the question type breakdown
+'''
+def create_pie_chart(question_types: dict[str, int], num_questions: int):
+    NUM_TO_KEEP = 15
+    sorted_types = [{"type": q, "count": c} for q,c in question_types.items()]
+    sorted_types.sort(key=lambda x: x["count"], reverse=True)
+    other_count = sum(x["count"] for x in sorted_types[NUM_TO_KEEP:])
+    sorted_types = sorted_types[0:NUM_TO_KEEP]
+    sorted_types.append({"type": "other", "count": other_count})
+    
+    _, _, autopcts = plt.pie([float(v['count']) for v in sorted_types], labels=[str(k['type']) for k in sorted_types], 
+            autopct = '%.0f%%',)
+    plt.setp(autopcts, **{'fontsize':8})
+    plt.axis('equal')
+    plt.show()
+    
+
 
 if __name__ == "__main__":
     # # Opening JSON file
@@ -43,9 +62,10 @@ if __name__ == "__main__":
     subset_annotations = json.load(f2)
 
     total_types, total_sum = generate_question_counts(all_annotations)
-    subset_types, subset_sum = generate_question_counts(subset_annotations)
+    # subset_types, subset_sum = generate_question_counts(subset_annotations)
 
-    print(generate_question_counts(all_annotations))
+    create_pie_chart(total_types, total_sum)
+    # print(generate_balanced_subset())
 
 
 
