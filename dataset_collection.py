f990da5d82d10fe5ad52ea6340f88b5a737f2d6b
Varshinee Sreekanth <varshinee@utexas.edu>
2022-05-03 10:17:43 -0500

    Fixed people question retrieval

--- dataset_collection.py
+++ dataset_collection.py
@@ -1,14 +1,18 @@
 import json
 import random
-from bisect import bisect_left
+from bisect import bisect_left, insort
 from pycocotools.coco import COCO
 import requests
 import os
 
-def binary_search(arr, tgt):
-    i = bisect_left(arr, tgt)
-    if i != len(arr) and arr[i] == tgt:
-        return i
+def binary_search(arr, tgt, key=None):
+    i = bisect_left(arr, tgt, key=(lambda x: x[key]) if key is not None else None)
+    if key is None:
+        if i != len(arr) and arr[i] == tgt:
+            return i
+    else:
+        if i != len(arr) and arr[i][key] == tgt:
+            return i
     return -1
 
 '''
@@ -53,11 +57,12 @@ Gets all the questions for each of these images and writes to a json file.
 Does not sort since we only need to do this once
 '''
 def retrieve_questions(img_ids, questions, file_name="test_questions.json"):
+    img_ids.sort()
     selected_questions = []
-    for i in range(len(questions)):
-        if any(img == questions[i]['image_id'] for img in img_ids):
+    for q in questions:
+        if binary_search(img_ids, q['image_id']) != -1:
             # we've picked this question, store it
-            selected_questions.append(questions[i])
+            selected_questions.append(q)
 
     # write to json file
     with open(file_name, "w") as f:
@@ -72,13 +77,12 @@ Gets all the corresponding annotations for each of these questions and writes to
 def retrieve_annotations(selected_questions, file_name="test_annotations.json"):
     f = open('./annotations/vqacp_v2_test_annotations.json')
     annotations = json.load(f)
+    annotations.sort(key=lambda x: x["question_id"])
 
     selected_annotations = []
-    for i in range(len(annotations)):
-        if any(selected_questions[j]['question_id'] == annotations[i]['question_id'] 
-                for j in range(len(selected_questions))):
-            # we've picked this question, store it
-            selected_annotations.append(annotations[i])
+    for q in selected_questions:
+        idx = binary_search(annotations, q["question_id"], key="question_id")
+        selected_annotations.append(annotations[idx])
 
     # write to json file
     with open(file_name, "w") as f:
@@ -132,18 +136,20 @@ def generate_image_ids_from_folder(folder_path, output_path):
 '''
 Counts proportion of people-based questions in the full dataset
 '''
-def select_people_questions(questions):
+def select_people_imgs(questions):
     person_words = ["person", "people", "man ", "woman", "men ", "women", "children", "child"]
-    selected_qs = []
+    img_ids = []
 
-    # pick out 150 images, with extras if we can't replace them manually
+    # picks image ids
     for q in questions:
         if (any(word in q["question"] for word in person_words) 
-                and q["image_id"] not in selected_qs):
-            selected_qs.append(q)
+                and binary_search(img_ids, q["image_id"]) == -1):
+            insort(img_ids, q["image_id"])
 
-    print(len(selected_qs) / len(questions))
-    return selected_qs
+    with open('./annotations/image_ids/people_image_ids.json', 'w') as img_file:
+        json.dump(img_ids, img_file)
+
+    return img_ids
 
 
 if __name__ == "__main__":
@@ -153,13 +159,14 @@ if __name__ == "__main__":
 
     # generate_image_ids(questions) # comment this out if text file already created
     # generate_image_ids_from_folder("./white_imgs", "white_image.json")
-    img_ids, img_splits = retrieve_img_ids("./annotations/image_ids/white_image_ids.json")
+    # img_ids, img_splits = retrieve_img_ids("./annotations/image_ids/white_image_ids.json")
     # download_images(img_ids, img_splits)
 
     # comment this in once images have been curated
-    selected_qs = retrieve_questions(img_ids, questions, "./annotations/subset_questions/white_questions.json")
-    # selected_qs = select_people_questions(questions)
-    retrieve_annotations(selected_qs, "./annotations/subset_annotations/white_annotations.json")
+    # selected_qs = retrieve_questions(img_ids, questions, "./annotations/subset_questions/white_questions.json")
+    selected_img_ids = select_people_imgs(questions)
+    selected_qs = retrieve_questions(selected_img_ids, questions, "./annotations/subset_questions/people_questions.json")
+    retrieve_annotations(selected_qs, "./annotations/subset_annotations/people_annotations.json")
     
     # TODO: Change the generate_image_ids method to read test_image_ids.json and generate images not in there to gather more data
 
--- question_analysis.py
+++ question_analysis.py
@@ -95,14 +95,14 @@ def create_histogram(dataset1: dict[str, int], dataset2: dict[str, int]):
     x = np.arange(15)
     bar_plot = plt.subplot(111)
     b1 = bar_plot.bar(x, tick_label=[t["type"] for t in sorted_types[:NUM_TO_KEEP]], 
-                 height = [t["count"] for t in sorted_types[:NUM_TO_KEEP]], color='black', align='center', width=w)
+                 height = [t["count"] for t in sorted_types[:NUM_TO_KEEP]], color='brown', align='center', width=w)
     b2 = bar_plot.bar(x+w, tick_label=[t["type"] for t in sorted_types2[:NUM_TO_KEEP]], 
-                 height = [t["count"] for t in sorted_types2[:NUM_TO_KEEP]], color='green', align='center', width=w)
+                 height = [t["count"] for t in sorted_types2[:NUM_TO_KEEP]], color='b', align='center', width=w)
     
     plt.xticks(rotation=20, ha='right')
-    bar_plot.legend((b1, b2), ('questions for mixed/unknown images', 'questions for nonwhite images'), loc='upper right')
+    bar_plot.legend((b1, b2), ('questions for all people images', 'questions for white images'), loc='upper right')
     bar_plot.set_ylabel('percentage of all question types')
-    # plt.show()
+    plt.show()
 
     return sorted_types, sorted_types2, sum1, sum2
 
@@ -130,20 +130,20 @@ if __name__ == "__main__":
     all_annotations = json.load(f)
     total_types = generate_question_counts(all_annotations)
 
-    # people_annotations = json.load(open('./all_people_annotations.json'))
-    # people_types = generate_question_counts(people_annotations, total_types)
+    people_annotations = json.load(open('./annotations/subset_annotations/people_annotations.json'))
+    people_types = generate_question_counts(people_annotations, total_types)
 
-    white_annotations = json.load(open('./white_annotations.json'))
+    white_annotations = json.load(open('./annotations/subset_annotations/white_annotations.json'))
     white_types = generate_question_counts(white_annotations, total_types)
     
-    nonwhite_annotations = json.load(open('./nonwhite_annotations.json'))
-    nonwhite_types = generate_question_counts(nonwhite_annotations, white_types)
+    # nonwhite_annotations = json.load(open('./annotations/subset_annotations/nonwhite_annotations.json'))
+    # nonwhite_types = generate_question_counts(nonwhite_annotations, white_types)
 
-    mixed_annotations = json.load(open('./mixed_annotations.json'))
-    mixed_types = generate_question_counts(mixed_annotations, white_types)
-
-    # create_pie_chart(nonwhite_types)
-    expected, observed, exp_sum, obs_sum = create_histogram(mixed_types, white_types)
+    # mixed_annotations = json.load(open('./mixed_annotations.json'))
+    # mixed_types = generate_question_counts(mixed_annotations, white_types)
+    print(len(people_annotations), len(people_annotations) / len(all_annotations))
+    # create_pie_chart(people_types)
+    expected, observed, exp_sum, obs_sum = create_histogram(people_types, white_types)
     calculate_chisquare(expected, observed, exp_sum, obs_sum)
     # print(generate_balanced_subset())
 
